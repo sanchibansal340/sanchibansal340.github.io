a8fbe0f552dc7147a45220abf7bd1ceb184076c8
Sanchi Bansal <sanchibansal340@gmail.com>
2021-03-12 18:20:03 +0530

    Change UI of website

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio Website
+
+- Built using HTML, CSS and JS.
+- Briefly explains about my skills, projects and experience.
--- main.js
+++ main.js
@@ -1,36 +1,3 @@
-// Vara for cusive animation in loader
-var fontSize = 72;
-
-if(window.screen.width > 700) 
-    fontSize = 32;
-        
-else if(window.screen.width < 1200) 
-    fontSize = 57;
-
-var vara = new Vara("#container", "https://raw.githubusercontent.com/akzhy/Vara/master/fonts/Satisfy/SatisfySL.json",
-    [
-        {
-            text:"Sanchi Bansal",
-            y:200,
-            duration:1500, 
-            textAlign:"center",
-            strokeWidth:2,
-            fontSize:fontSize, 
-        },
-    ]
-);
-
-// Remove the loader(cursive) and show main page
-$(window).on('load', function(){
-    setTimeout(removeLoader, 2500);
-  });
-  function removeLoader(){
-      $( "#container" ).fadeOut(500, function() {
-        $( "#container" ).remove();
-        $("#page-container").show()
-    });  
-}
-
 // Go to Top Button
 const goToTopBtn = document.getElementById("goToTop");
 
