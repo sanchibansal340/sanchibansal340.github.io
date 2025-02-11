a68383e221d0ad466686576f439e2e842c8f83b5
Sanchi Bansal <sanchibansal340@gmail.com>
2025-02-12 00:31:13 +0530

    add. prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Landing, About } from "./components";
+import { Landing, About } from './components'
 
 function App() {
-  return (
-    <div className="App">
-      <Landing />
-      <About/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Landing />
+            <About />
+        </div>
+    )
 }
 
-export default App;
+export default App
